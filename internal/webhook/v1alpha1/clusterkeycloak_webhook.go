dfd41ab7549dc146255179763faeb0d01c18ae2a
Thomas Boerger <thomas@webhippie.de>
2026-02-08 17:12:03 +0100

    feat: finish validating webhook for keycloak instances

--- internal/webhook/v1alpha1/clusterkeycloak_webhook.go
+++ internal/webhook/v1alpha1/clusterkeycloak_webhook.go
@@ -19,19 +19,17 @@ package v1alpha1
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	"github.com/kubehippie/keycloak-operator/api/common"
 	keycloakoperatorwebhippiedev1alpha1 "github.com/kubehippie/keycloak-operator/api/v1alpha1"
 )
-
-// nolint:unused
-// log is for logging in this package.
-var clusterkeycloaklog = logf.Log.WithName("clusterkeycloak-resource")
 
 // SetupClusterKeycloakWebhookWithManager registers the webhook for ClusterKeycloak in the manager.
 func SetupClusterKeycloakWebhookWithManager(mgr ctrl.Manager) error {
@@ -41,8 +39,6 @@ func SetupClusterKeycloakWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 // +kubebuilder:webhook:path=/mutate-keycloak-operator-webhippie-de-v1alpha1-clusterkeycloak,mutating=true,failurePolicy=fail,sideEffects=None,groups=keycloak-operator.webhippie.de,resources=clusterkeycloaks,verbs=create;update,versions=v1alpha1,name=mclusterkeycloak-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // ClusterKeycloakCustomDefaulter struct is responsible for setting default values on the custom resource of the
@@ -50,28 +46,15 @@ func SetupClusterKeycloakWebhookWithManager(mgr ctrl.Manager) error {
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
-type ClusterKeycloakCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
-}
+type ClusterKeycloakCustomDefaulter struct{}
 
 var _ webhook.CustomDefaulter = &ClusterKeycloakCustomDefaulter{}
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind ClusterKeycloak.
 func (d *ClusterKeycloakCustomDefaulter) Default(_ context.Context, obj runtime.Object) error {
-	clusterkeycloak, ok := obj.(*keycloakoperatorwebhippiedev1alpha1.ClusterKeycloak)
-
-	if !ok {
-		return fmt.Errorf("expected an ClusterKeycloak object but got %T", obj)
-	}
-	clusterkeycloaklog.Info("Defaulting for ClusterKeycloak", "name", clusterkeycloak.GetName())
-
-	// TODO(user): fill in your defaulting logic.
-
 	return nil
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// NOTE: If you want to customise the 'path', use the flags '--defaulting-path' or '--validation-path'.
 // +kubebuilder:webhook:path=/validate-keycloak-operator-webhippie-de-v1alpha1-clusterkeycloak,mutating=false,failurePolicy=fail,sideEffects=None,groups=keycloak-operator.webhippie.de,resources=clusterkeycloaks,verbs=create;update,versions=v1alpha1,name=vclusterkeycloak-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // ClusterKeycloakCustomValidator struct is responsible for validating the ClusterKeycloak resource
@@ -79,9 +62,7 @@ func (d *ClusterKeycloakCustomDefaulter) Default(_ context.Context, obj runtime.
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
-type ClusterKeycloakCustomValidator struct {
-	// TODO(user): Add more fields as needed for validation
-}
+type ClusterKeycloakCustomValidator struct{}
 
 var _ webhook.CustomValidator = &ClusterKeycloakCustomValidator{}
 
@@ -91,11 +72,8 @@ func (v *ClusterKeycloakCustomValidator) ValidateCreate(_ context.Context, obj r
 	if !ok {
 		return nil, fmt.Errorf("expected a ClusterKeycloak object but got %T", obj)
 	}
-	clusterkeycloaklog.Info("Validation for ClusterKeycloak upon creation", "name", clusterkeycloak.GetName())
 
-	// TODO(user): fill in your validation logic upon object creation.
-
-	return nil, nil
+	return nil, v.validate(clusterkeycloak)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type ClusterKeycloak.
@@ -104,22 +82,53 @@ func (v *ClusterKeycloakCustomValidator) ValidateUpdate(_ context.Context, oldOb
 	if !ok {
 		return nil, fmt.Errorf("expected a ClusterKeycloak object for the newObj but got %T", newObj)
 	}
-	clusterkeycloaklog.Info("Validation for ClusterKeycloak upon update", "name", clusterkeycloak.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return nil, v.validate(clusterkeycloak)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type ClusterKeycloak.
 func (v *ClusterKeycloakCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	clusterkeycloak, ok := obj.(*keycloakoperatorwebhippiedev1alpha1.ClusterKeycloak)
-	if !ok {
-		return nil, fmt.Errorf("expected a ClusterKeycloak object but got %T", obj)
-	}
-	clusterkeycloaklog.Info("Validation for ClusterKeycloak upon deletion", "name", clusterkeycloak.GetName())
-
-	// TODO(user): fill in your validation logic upon object deletion.
-
 	return nil, nil
+}
+
+func (v *ClusterKeycloakCustomValidator) validate(clusterkeycloak *keycloakoperatorwebhippiedev1alpha1.ClusterKeycloak) error {
+	spec := clusterkeycloak.Spec
+
+	parsed, err := url.ParseRequestURI(
+		strings.TrimSpace(spec.URL),
+	)
+
+	if err != nil || parsed.Scheme == "" || parsed.Host == "" {
+		return fmt.Errorf("spec.url must be a valid URL")
+	}
+
+	if strings.TrimSpace(spec.RealmName) == "" {
+		return fmt.Errorf("spec.realmName must be set")
+	}
+
+	if err := v.validateSecretKeyRefOrVal("spec.username", spec.Username); err != nil {
+		return err
+	}
+
+	if err := v.validateSecretKeyRefOrVal("spec.password", spec.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *ClusterKeycloakCustomValidator) validateSecretKeyRefOrVal(field string, ref *common.SecretKeyRefOrVal) error {
+	if ref == nil {
+		return fmt.Errorf("%s must be set", field)
+	}
+
+	if strings.TrimSpace(ref.Value) != "" {
+		return nil
+	}
+
+	if strings.TrimSpace(ref.Name) == "" || strings.TrimSpace(ref.Key) == "" {
+		return fmt.Errorf("%s must set value or secret name/key", field)
+	}
+
+	return nil
 }
--- internal/webhook/v1alpha1/keycloak_webhook.go
+++ internal/webhook/v1alpha1/keycloak_webhook.go
@@ -19,19 +19,17 @@ package v1alpha1
 import (
 	"context"
 	"fmt"
+	"net/url"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
+	"github.com/kubehippie/keycloak-operator/api/common"
 	keycloakoperatorwebhippiedev1alpha1 "github.com/kubehippie/keycloak-operator/api/v1alpha1"
 )
-
-// nolint:unused
-// log is for logging in this package.
-var keycloaklog = logf.Log.WithName("keycloak-resource")
 
 // SetupKeycloakWebhookWithManager registers the webhook for Keycloak in the manager.
 func SetupKeycloakWebhookWithManager(mgr ctrl.Manager) error {
@@ -41,8 +39,6 @@ func SetupKeycloakWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
 // +kubebuilder:webhook:path=/mutate-keycloak-operator-webhippie-de-v1alpha1-keycloak,mutating=true,failurePolicy=fail,sideEffects=None,groups=keycloak-operator.webhippie.de,resources=keycloaks,verbs=create;update,versions=v1alpha1,name=mkeycloak-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // KeycloakCustomDefaulter struct is responsible for setting default values on the custom resource of the
@@ -50,28 +46,15 @@ func SetupKeycloakWebhookWithManager(mgr ctrl.Manager) error {
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
-type KeycloakCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
-}
+type KeycloakCustomDefaulter struct{}
 
 var _ webhook.CustomDefaulter = &KeycloakCustomDefaulter{}
 
 // Default implements webhook.CustomDefaulter so a webhook will be registered for the Kind Keycloak.
 func (d *KeycloakCustomDefaulter) Default(_ context.Context, obj runtime.Object) error {
-	keycloak, ok := obj.(*keycloakoperatorwebhippiedev1alpha1.Keycloak)
-
-	if !ok {
-		return fmt.Errorf("expected an Keycloak object but got %T", obj)
-	}
-	keycloaklog.Info("Defaulting for Keycloak", "name", keycloak.GetName())
-
-	// TODO(user): fill in your defaulting logic.
-
 	return nil
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// NOTE: If you want to customise the 'path', use the flags '--defaulting-path' or '--validation-path'.
 // +kubebuilder:webhook:path=/validate-keycloak-operator-webhippie-de-v1alpha1-keycloak,mutating=false,failurePolicy=fail,sideEffects=None,groups=keycloak-operator.webhippie.de,resources=keycloaks,verbs=create;update,versions=v1alpha1,name=vkeycloak-v1alpha1.kb.io,admissionReviewVersions=v1
 
 // KeycloakCustomValidator struct is responsible for validating the Keycloak resource
@@ -79,9 +62,7 @@ func (d *KeycloakCustomDefaulter) Default(_ context.Context, obj runtime.Object)
 //
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
-type KeycloakCustomValidator struct {
-	// TODO(user): Add more fields as needed for validation
-}
+type KeycloakCustomValidator struct{}
 
 var _ webhook.CustomValidator = &KeycloakCustomValidator{}
 
@@ -91,11 +72,8 @@ func (v *KeycloakCustomValidator) ValidateCreate(_ context.Context, obj runtime.
 	if !ok {
 		return nil, fmt.Errorf("expected a Keycloak object but got %T", obj)
 	}
-	keycloaklog.Info("Validation for Keycloak upon creation", "name", keycloak.GetName())
 
-	// TODO(user): fill in your validation logic upon object creation.
-
-	return nil, nil
+	return nil, v.validate(keycloak)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type Keycloak.
@@ -104,22 +82,53 @@ func (v *KeycloakCustomValidator) ValidateUpdate(_ context.Context, oldObj, newO
 	if !ok {
 		return nil, fmt.Errorf("expected a Keycloak object for the newObj but got %T", newObj)
 	}
-	keycloaklog.Info("Validation for Keycloak upon update", "name", keycloak.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return nil, v.validate(keycloak)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type Keycloak.
 func (v *KeycloakCustomValidator) ValidateDelete(ctx context.Context, obj runtime.Object) (admission.Warnings, error) {
-	keycloak, ok := obj.(*keycloakoperatorwebhippiedev1alpha1.Keycloak)
-	if !ok {
-		return nil, fmt.Errorf("expected a Keycloak object but got %T", obj)
-	}
-	keycloaklog.Info("Validation for Keycloak upon deletion", "name", keycloak.GetName())
-
-	// TODO(user): fill in your validation logic upon object deletion.
-
 	return nil, nil
+}
+
+func (v *KeycloakCustomValidator) validate(keycloak *keycloakoperatorwebhippiedev1alpha1.Keycloak) error {
+	spec := keycloak.Spec
+
+	parsed, err := url.ParseRequestURI(
+		strings.TrimSpace(spec.URL),
+	)
+
+	if err != nil || parsed.Scheme == "" || parsed.Host == "" {
+		return fmt.Errorf("spec.url must be a valid URL")
+	}
+
+	if strings.TrimSpace(spec.RealmName) == "" {
+		return fmt.Errorf("spec.realmName must be set")
+	}
+
+	if err := v.validateSecretKeyRefOrVal("spec.username", spec.Username); err != nil {
+		return err
+	}
+
+	if err := v.validateSecretKeyRefOrVal("spec.password", spec.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (v *KeycloakCustomValidator) validateSecretKeyRefOrVal(field string, ref *common.SecretKeyRefOrVal) error {
+	if ref == nil {
+		return fmt.Errorf("%s must be set", field)
+	}
+
+	if strings.TrimSpace(ref.Value) != "" {
+		return nil
+	}
+
+	if strings.TrimSpace(ref.Name) == "" || strings.TrimSpace(ref.Key) == "" {
+		return fmt.Errorf("%s must set value or secret name/key", field)
+	}
+
+	return nil
 }
